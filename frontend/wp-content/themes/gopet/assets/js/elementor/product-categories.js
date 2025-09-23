e8a14274c4ee2d5422293e90f88424a795674441
白bayai <qq1795555655@163.com>
2025-09-23 20:02:30 +0800

    first commit

---
+++ frontend/wp-content/themes/gopet/assets/js/elementor/product-categories.js
@@ -0,0 +1,118 @@
+﻿(function ($) {
+    "use strict";
+    $(window).on('elementor/frontend/init', () => {
+        elementorFrontend.hooks.addAction('frontend/element_ready/gopet-product-categories.default', ($scope) => {
+            let $carousel = $('.gopet-carousel', $scope);
+            if ($carousel.length > 0) {
+                let data = $carousel.data('settings');
+                if (data['layout'] === '1') {
+                    $carousel.slick(
+                        {
+                            dots: data.navigation == 'both' || data.navigation == 'dots' ? true : false,
+                            arrows: data.navigation == 'both' || data.navigation == 'arrows' ? true : false,
+                            infinite: data.loop,
+                            speed: 300,
+                            slidesToShow: parseInt(data.items),
+                            autoplay: data.autoplay,
+                            autoplaySpeed: data.autoplaySpeed,
+                            slidesToScroll: 1,
+                            lazyLoad: 'ondemand',
+                            rtl: data.rtl,
+                            centerMode: true,
+                            centerPadding: '110px',
+                            responsive: [
+                                {
+                                    breakpoint: parseInt(data.breakpoint_laptop),
+                                    settings: {
+                                        slidesToShow: parseInt(data.items_laptop),
+                                        centerPadding: '100px',
+                                    }
+                                },
+                                {
+                                    breakpoint: parseInt(data.breakpoint_tablet_extra),
+                                    settings: {
+                                        slidesToShow: parseInt(data.items_tablet_extra),
+                                        centerPadding: '80px',
+                                    }
+                                },
+                                {
+                                    breakpoint: parseInt(data.breakpoint_tablet),
+                                    settings: {
+                                        slidesToShow: parseInt(data.items_tablet),
+                                        centerMode: false,
+                                        centerPadding: '0px',
+                                    }
+                                },
+                                {
+                                    breakpoint: parseInt(data.breakpoint_mobile_extra),
+                                    settings: {
+                                        slidesToShow: parseInt(data.items_mobile_extra),
+                                        centerMode: false,
+                                        centerPadding: '0px',
+                                    }
+                                },
+                                {
+                                    breakpoint: parseInt(data.breakpoint_mobile),
+                                    settings: {
+                                        slidesToShow: parseInt(data.items_mobile),
+                                        centerMode: false,
+                                        centerPadding: '0px',
+                                    }
+                                }
+                            ]
+                        }
+                    );
+                } else {
+                    $carousel.slick(
+                        {
+                            dots: data.navigation == 'both' || data.navigation == 'dots' ? true : false,
+                            arrows: data.navigation == 'both' || data.navigation == 'arrows' ? true : false,
+                            infinite: data.loop,
+                            speed: 300,
+                            slidesToShow: parseInt(data.items),
+                            autoplay: data.autoplay,
+                            autoplaySpeed: data.autoplaySpeed,
+                            slidesToScroll: 1,
+                            lazyLoad: 'ondemand',
+                            rtl: data.rtl,
+                            responsive: [
+                                {
+                                    breakpoint: parseInt(data.breakpoint_laptop),
+                                    settings: {
+                                        slidesToShow: parseInt(data.items_laptop),
+                                    }
+                                },
+                                {
+                                    breakpoint: parseInt(data.breakpoint_tablet_extra),
+                                    settings: {
+                                        slidesToShow: parseInt(data.items_tablet_extra),
+                                    }
+                                },
+                                {
+                                    breakpoint: parseInt(data.breakpoint_tablet),
+                                    settings: {
+                                        slidesToShow: parseInt(data.items_tablet),
+                                    }
+                                },
+                                {
+                                    breakpoint: parseInt(data.breakpoint_mobile_extra),
+                                    settings: {
+                                        slidesToShow: parseInt(data.items_mobile_extra),
+                                    }
+                                },
+                                {
+                                    breakpoint: parseInt(data.breakpoint_mobile),
+                                    settings: {
+                                        slidesToShow: parseInt(data.items_mobile),
+                                    }
+                                }
+                            ]
+                        }
+                    );
+                }
+            }
+        });
+    });
+})(jQuery);
+
+
